b25e57134209f3122c92ace8799512030b3f623d
Kiran Gautam <ukiras@gmail.com>
2025-08-03 19:36:55 +1000

    remove dist

--- dist/src/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AppService } from './app.service';
-import { Response } from 'express';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    serveFloorplan(filename: string, res: Response): void;
-}
