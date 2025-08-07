2d078dbb29a0d7c2a639f81a0ea87c32005a0d54
Kiran Gautam <ukiras@gmail.com>
2025-08-07 23:34:47 +1000

    fix endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,8 +8,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHealth() {
+    return this.appService.getHealth();
   }
 
   // @Get('test-floorplan')
