b4c709d1e9b13066ce21640fae3f0bc9dc59a7a4
lakpa.lama <lakpa.lama@worldlink.com.np>
2025-08-29 16:02:07 +0545

    entry file change

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,6 +11,9 @@ echo "➡️ Running Prisma Seed..."
 # npx tsx prisma/seeds/lot.ts
 echo "✅ Seeding complete."
 
+npx tsx prisma/seeds/geoData.ts
+npx tsx prisma/seeds/lot.ts
+
 # If you want to reset the database, you can run the following commands:
 # npx prisma migrate reset --force
 # npx tsx prisma/seeds/seed.ts
