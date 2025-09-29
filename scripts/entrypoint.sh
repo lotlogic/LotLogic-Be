10f68a4219e67a868d6dc575afd4f703645dabe8
Kiran Gautam <ukiras@gmail.com>
2025-09-29 21:11:31 +1000

    fix the seed issue

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,18 +6,14 @@ echo "➡️ Running Prisma Migrations..."
 npx prisma migrate deploy
 echo "✅ Migrations complete."
 
-echo "➡️ Running Prisma Seed..."
-# npx tsx prisma/seeds/seed.ts
-# npx tsx prisma/seeds/lot.ts
-echo "✅ Seeding complete."
-
-npx tsx prisma/seeds/geoData.ts
-npx tsx prisma/seeds/lot.ts
-
+# NOTE: DO NOT UNCOMMENT THESE SEEDS, this will reset the database and seed the data again
 # If you want to reset the database, you can run the following commands:
 # npx prisma migrate reset --force
+# echo "➡️ Running Prisma Seed..."
 # npx tsx prisma/seeds/seed.ts
 # npx tsx prisma/seeds/lot.ts
+# npx tsx prisma/seeds/geoData.ts
+# echo "✅ Seeding complete."
 
 echo "🚀 Starting NestJS app..."
 exec node dist/src/main.js
