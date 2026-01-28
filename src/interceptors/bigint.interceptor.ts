7ca2387689d4226ddce8eecb8ad0b17f959aade5
Henry Tait <mossy@interesting.com.au>
2026-01-28 13:06:38 +0800

    bigint date handler

--- src/interceptors/bigint.interceptor.ts
+++ src/interceptors/bigint.interceptor.ts
@@ -8,6 +8,9 @@ export class BigIntInterceptor implements NestInterceptor {
     if (typeof value === 'bigint') {
       return value.toString();
     }
+    if (value instanceof Date) {
+      return value.toISOString();
+    }
     if (Array.isArray(value)) {
       return value.map(item => this.transformValue(item));
     }
